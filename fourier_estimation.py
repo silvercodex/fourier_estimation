76d3d1c6bd030627fe7895f15d59025cb18ec051
silvercodex <cody.harmon90@gmail.com>
2019-05-19 09:34:22 -0500

    Update fourier_estimation.py

--- fourier_estimation.py
+++ fourier_estimation.py
@@ -18,16 +18,21 @@ class Fourier_Estimator:
         fourier_coef = fourier_data.mean(axis = 1)
         fourier_coef.iloc[0] = 1
         x = sympy.symbols("x")
-        pdf = self.pdf_fun(data,min_x,max_x)
+        coef = np.array(fourier_coef).reshape(1,-1)
+        funcs = np.array(pd.Series(range(len(fourier_coef))).apply(lambda i: self.cos_fun2(i,x,self.min_x,self.max_x))).reshape(-1,1)
+        pdf = np.dot(coef,funcs)[0][0]
         self.pdf = sympy.Max(0,pdf)
         self.norm_const()
+        
         
     def get_curve(self, uniform = True, data = None, n= 500):
         if uniform:
             l = np.linspace(self.min_x,self.max_x,n)
-            return pd.Series(l).apply(lambda y: float(self.pdf.subs(x,y)))
+            temp = sympy.lambdify(x,self.pdf,"numpy")
+            return pd.Series(l).apply(lambda y: float(temp(y)))
         else:
-            return data.apply(lambda y: float(self.pdf.subs(x,y)))
+            temp = sympy.lambdify(x,self.pdf,"numpy")
+            return data.apply(lambda y: float(temp(y)))
             
     def norm_const(self, n=  500):
         if self.pdf == None:
@@ -35,7 +40,8 @@ class Fourier_Estimator:
         else:
             l = np.linspace(self.min_x,self.max_x,n)
             length = l[1]-l[0]
-            pdf_l = pd.Series(l).apply(lambda y: float(self.pdf.subs(x,y)))
+            temp = sympy.lambdify(x,self.pdf,"numpy")
+            pdf_l = pd.Series(l).apply(lambda y:  float(temp(y)))
             self.norm = (pdf_l*length).sum()
             
     def cos_fun(self,j,x,min_x,max_x):
@@ -49,8 +55,3 @@ class Fourier_Estimator:
             return 1/(max_x-min_x)
         else:
             return sympy.sqrt(2/(max_x-min_x))*sympy.cos(pi*j*x/(max_x-min_x))   
-    
-    def pdf_fun(self,x,min_x,max_x):
-        coef = np.array(fourier_coef).reshape(1,-1)
-        funcs = np.array(pd.Series(range(len(fourier_coef))).apply(lambda i: cos_fun2(i,x,min_x,max_x))).reshape(-1,1)
-        return np.dot(coef,funcs)[0][0]
